0ad64856524292ece233faf35f65054d5bbf9e02
hanbyeol <hanbyeol4869@gmail.com>
2022-01-07 13:45:20 +0900

    OK

---
+++ 1008.C
@@ -0,0 +1,13 @@
+//#1008. A/B 출력하기
+
+#include <stdio.h>
+
+int main(){
+
+    double a, b;
+
+    scanf("%lf %lf", &a, &b);
+    printf("%lf", a / b);
+
+    return 0;
+}
---
+++ 10869.C
@@ -0,0 +1,18 @@
+//10869. 사칙연산
+
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%d\n", a / b);
+    printf("%d", a % b);
+
+    return 0;
+}
